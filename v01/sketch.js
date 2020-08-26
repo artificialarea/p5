a8c268b5834c7adcbe64f10e119c658243d3411c
Sacha Sedriks <sachased@gmail.com>
2020-08-26 13:30:05 -0700

    Add: dropdown menu to setType of noise; volume slider; requisite event listeners for change events

--- v01/sketch.js
+++ v01/sketch.js
@@ -1,20 +1,46 @@
 /* eslint-disable indent */
 
-// conventions : global declarations upfront, 
-// then do global assignments in `setup` function
+// convention: declare first, assign and init later.
 
-let mrNoisy;                    // noise object
-let playButton, stopButton;     // DOM element
+// declare...
+// noise object
+let mrNoisy;      
+// DOM element              
+let playButton, stopButton, chooseNoise, setVolume;    
 
+// assign & init...
 function setup() {
-    // assign & init p5 sound object
-    mrNoisy = new p5.Noise('brown');
-    mrNoisy.amp(0.1);
 
-    // assign & init buttons
+    // p5 sound object
+    mrNoisy = new p5.Noise();
+    mrNoisy.amp(0.1);
+    
+    // controls
+    chooseNoise = createSelect();
+    chooseNoise.position(60, 10);
+    chooseNoise.option('white');
+    chooseNoise.option('pink');
+    chooseNoise.option('brown');
+    chooseNoise.changed(() => {
+        mrNoisy.setType(chooseNoise.value());
+    });
+
+    setVolume = createSlider(0, 1, 0.1, 0);     // createSlider(min, max, [value], [step])
+    setVolume.position(130, 10);
+    setVolume.input(() => {
+        mrNoisy.amp(setVolume.value(), 0.01);   // amp([vol], [slewTime], [tFromNow])
+        // console.log(setVolume.value());
+    });
+
     playButton = createButton('play');
     playButton.position(10, 10);
     playButton.mousePressed(() => mrNoisy.start());  
-    // chained methods variant
-    stopButton = createButton('stop').position(60, 10).mousePressed(() => mrNoisy.stop());
+    // note: chained methods variant
+    stopButton = createButton('stop').position(10, 40).mousePressed(() => mrNoisy.stop());
 }
+
+// my intial attempt used default draw function
+// before learning .input() event listener method (above)
+// function draw() {
+//     mrNoisy.amp(setVolume.value() / 100);   // amplitude between 0 and 1.0
+// }
